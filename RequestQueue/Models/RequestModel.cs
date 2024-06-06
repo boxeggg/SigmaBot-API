1b7141525b8ab27846a3865b9504fbc6b1e34e83
boxeggg <kamilszydlowski53@gmail.com>
2024-06-06 18:35:30 +0200

    Dodaj pliki projektów.

---
+++ RequestQueue/Models/RequestModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RequestQueue.Models
+{
+    public class RequestModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public  string Url { get; set; }
+        public string User {  get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+
+
+    }
+}
