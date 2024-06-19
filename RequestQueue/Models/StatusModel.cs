13dad41b131daa26ee777b2151ad97439002ed9d
boxeggg <kamilszydlowski53@gmail.com>
2024-06-20 00:21:22 +0200

    new fields in status model

--- RequestQueue/Models/StatusModel.cs
+++ RequestQueue/Models/StatusModel.cs
@@ -2,14 +2,24 @@
 {
     public class StatusModel
     {
+        public string GuildId { get; set; }
+        public LoopModes LoopMode { get; set; }
         public bool OnVoiceChannel { get; set; } 
         public double Volume { get; set; } 
         public bool SkipQueued { get; set; }  
-        public StatusModel() { 
+        public StatusModel() {
+            GuildId = "";
+            LoopMode = LoopModes.None;
             OnVoiceChannel = false;
             Volume = 50;
             SkipQueued = false;
         }
         
+    }
+    public enum LoopModes
+    {
+        None = 0,
+        Song = 1,
+        Queue = 2,
     }
 }
